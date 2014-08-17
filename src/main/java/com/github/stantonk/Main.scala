2c3a29c22e9c97b558f5611c1c609e02b8c364e2
Kevin Stanton <stantonk@gmail.com>
2014-08-17 02:30:46 -0500

    general method for returning text http responses

--- src/main/java/com/github/stantonk/Main.scala
+++ src/main/java/com/github/stantonk/Main.scala
@@ -3,16 +3,19 @@ package com.github.stantonk
 import com.twitter.util.{Future, Await}
 import org.jboss.netty.handler.codec.http.{HttpRequest, HttpResponse, DefaultHttpResponse, HttpResponseStatus}
 import com.twitter.finagle.{Http, Service}
-import org.jboss.netty.buffer.ChannelBuffers
+import org.jboss.netty.buffer.ChannelBuffers.copiedBuffer
 
 object Main extends App {
 
+  def renderResponse(req : HttpRequest, content: String) : DefaultHttpResponse = {
+    val resp = new DefaultHttpResponse(req.getProtocolVersion, HttpResponseStatus.OK)
+    resp.setContent(copiedBuffer(content.getBytes))
+    resp
+  }
+
   val service = new Service[HttpRequest, HttpResponse] {
     def apply(req: HttpRequest): Future[HttpResponse] = {
-      val resp = new DefaultHttpResponse(req.getProtocolVersion, HttpResponseStatus.OK)
-      val b = ChannelBuffers.copiedBuffer("hello world".getBytes())
-      resp.setContent(b)
-      Future.value(resp)
+      Future.value(renderResponse(req, "Hello World!"))
     }
   }
   val server = Http.serve(":8080", service)
